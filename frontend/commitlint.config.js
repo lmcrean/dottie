ba311f59fa4402e27219a799ded3663f9eded108
Aznim Rahman <aznimr@protonmail.com>
2025-04-26 14:43:48 +0100

    uninstall husky

--- frontend/commitlint.config.js
+++ frontend/commitlint.config.js
@@ -1,1 +1,17 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Enforce type to be one of these
+    'type-enum': [
+      2, // Error level
+      'always',
+      [
+        'feat', // A new feature
+        'fix' // A bug fix
+      ]
+    ],
+
+    // Subject should not start with a capital letter
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']]
+  }
+};
--- frontend/lint-staged.config.js
+++ frontend/lint-staged.config.js
@@ -1,3 +1,5 @@
 export default {
-  '**/*.{ts,tsx}': (stagedFiles) => [`eslint .`, `prettier --write ${stagedFiles.join(' ')}`]
+  '*.ts': 'eslint --fix',
+  '*.tsx': 'eslint --fix',
+  '*.{ts,tsx,js,jsx,json,css,md}': 'prettier --write'
 };
