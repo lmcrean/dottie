83d0e761f698c344c5023098148c004435fc9d76
Aznim Rahman <aznimr@protonmail.com>
2025-04-26 11:03:19 +0100

    testing husky again

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -28,7 +28,7 @@ export default defineConfig([
     plugins: { js, 'react-hooks': reactHooks, 'react-refresh': reactRefresh },
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended,
+      tseslint.configs.recommended,
       pluginReact.configs.flat.recommended,
       jsxA11y.flatConfigs.recommended,
       prettier
@@ -38,24 +38,18 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
         tsconfigRootDir: import.meta.dirname
       }
     },
     settings: {
       react: {
         version: 'detect'
-      },
-      'import/resolver': {
-        typescript: {
-          project: './tsconfig.json'
-        }
       }
     },
     rules: {
       'no-unused-vars': 'error',
       'no-undef': 'error',
-      // 'no-console': 'warn',
+      'no-console': 'warn',
       'react/button-has-type': 'error',
       'react/react-in-jsx-scope': ['off'],
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
